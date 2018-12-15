555f288846b267ed2c994aae9de4258c32b975b6
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-15 21:41:45 +0200

    save user input for invalid signup form

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,13 +39,27 @@ exports.getSignup = (req, res, next) => {
 	res.render("auth/signup", {
 		path: "/signup",
 		pageTitle: "Signup",
-		errorMessage: message
+		errorMessage: message,
+		oldInput: {
+			email: "",
+			password: "",
+			confirmPassword: ""
+		}
 	});
 };
 
 exports.postLogin = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).render("auth/login", {
+			path: "/login",
+			pageTitle: "Login",
+			errorMessage: errors.array()[0].msg
+		});
+	}
+
 	User.findOne({ email })
 		.then(user => {
 			if (!user) {
@@ -82,7 +96,8 @@ exports.postSignup = (req, res, next) => {
 		return res.status(422).render("auth/signup", {
 			path: "/signup",
 			pageTitle: "Signup",
-			errorMessage: errors.array()[0].msg
+			errorMessage: errors.array()[0].msg,
+			oldInput: { email, password, confirmPassword: req.body.confirmPassword }
 		});
 	}
 
