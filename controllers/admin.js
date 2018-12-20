29d179792008dc85c54d2bf16f01a280f9a16a81
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-20 23:36:18 +0200

    handle image upload

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,9 +14,24 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
 	const title = req.body.title;
-	const imageUrl = req.body.imageUrl;
+	const image = req.file;
 	const price = req.body.price;
 	const description = req.body.description;
+	if (!image) {
+		return res.status(422).render("admin/edit-product", {
+			pageTitle: "Add Product",
+			path: "/admin/add-product",
+			editing: false,
+			product: {
+				title,
+				price,
+				description
+			},
+			hasError: true,
+			errorMessage: "Attached file is not an image",
+			validationErrors: []
+		});
+	}
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(422).render("admin/edit-product", {
@@ -25,7 +40,6 @@ exports.postAddProduct = (req, res, next) => {
 			editing: false,
 			product: {
 				title,
-				imageUrl,
 				price,
 				description
 			},
@@ -34,6 +48,9 @@ exports.postAddProduct = (req, res, next) => {
 			validationErrors: errors.array()
 		});
 	}
+
+	const imageUrl = image.path;
+
 	const product = new Product({
 		title: title,
 		price: price,
@@ -45,7 +62,6 @@ exports.postAddProduct = (req, res, next) => {
 		.save()
 		.then(result => {
 			// console.log(result);
-			console.log("Created Product");
 			res.redirect("/admin/products");
 		})
 		.catch(err => {
@@ -63,7 +79,7 @@ exports.postAddProduct = (req, res, next) => {
 			// 	errorMessage: "Database operation failed, please try again",
 			// 	validationErrors: []
 			// });
-			// res.redirect("/500");
+			res.redirect("/500");
 			const error = new Error(err);
 			error.httpStatusCode = 500;
 			return next(error);
@@ -102,19 +118,17 @@ exports.postEditProduct = (req, res, next) => {
 	const prodId = req.body.productId;
 	const updatedTitle = req.body.title;
 	const updatedPrice = req.body.price;
-	const updatedImageUrl = req.body.imageUrl;
+	const image = req.file;
 	const updatedDesc = req.body.description;
 
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		console.log(errors.array());
 		return res.status(422).render("admin/edit-product", {
 			pageTitle: "Edit Product",
 			path: "/admin/add-product",
 			editing: true,
 			product: {
 				title: updatedTitle,
-				imageUrl: updatedImageUrl,
 				price: updatedPrice,
 				description: updatedDesc,
 				_id: prodId
@@ -133,7 +147,9 @@ exports.postEditProduct = (req, res, next) => {
 			product.title = updatedTitle;
 			product.price = updatedPrice;
 			product.description = updatedDesc;
-			product.imageUrl = updatedImageUrl;
+			if (image) {
+				product.imageUrl = image.path;
+			}
 			return product.save().then(result => {
 				console.log("UPDATED PRODUCT!");
 				res.redirect("/admin/products");
@@ -151,7 +167,6 @@ exports.getProducts = (req, res, next) => {
 		// .select('title price -_id')
 		// .populate('userId', 'name')
 		.then(products => {
-			console.log(products);
 			res.render("admin/products", {
 				prods: products,
 				pageTitle: "Admin Products",
