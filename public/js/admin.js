360ff2bb7db5ef082e8e3d6e8fdb082344a37f2f
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-21 15:05:35 +0200

    remove deleted product from the dom

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,7 @@
 const deleteProduct = btn => {
 	const productId = btn.parentNode.querySelector("[name=productId]").value;
 	const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
-
+	const productElement = btn.closest("article");
 	fetch(`/admin/product/${productId}`, {
 		method: "DELETE",
 		headers: {
@@ -9,7 +9,11 @@ const deleteProduct = btn => {
 		}
 	})
 		.then(result => {
-			console.log(result);
+			return result.json();
+		})
+		.then(data => {
+			console.log(data);
+			productElement.parentNode.removeChild(productElement);
 		})
 		.catch(err => {
 			console.log(err);
