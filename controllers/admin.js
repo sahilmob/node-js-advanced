d7f1d98ca6c638370bf4a6ab6f7e7ed6220ac638
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-21 15:01:18 +0200

    handle async delete

--- controllers/admin.js
+++ controllers/admin.js
@@ -182,8 +182,8 @@ exports.getProducts = (req, res, next) => {
 		});
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-	const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+	const prodId = req.params.productId;
 	Product.findById(prodId)
 		.then(prod => {
 			if (!prod) {
@@ -194,11 +194,34 @@ exports.postDeleteProduct = (req, res, next) => {
 		})
 		.then(() => {
 			console.log("DESTROYED PRODUCT");
-			res.redirect("/admin/products");
+			res.status(200).json({
+				message: "Success!"
+			});
 		})
 		.catch(err => {
-			const error = new Error(err);
-			error.httpStatusCode = 500;
-			return next(error);
+			res.status(500).json({
+				message: "Deleting product failed"
+			});
 		});
 };
+
+// exports.postDeleteProduct = (req, res, next) => {
+// 	const prodId = req.body.productId;
+// 	Product.findById(prodId)
+// 		.then(prod => {
+// 			if (!prod) {
+// 				return next(new Error("Product not found"));
+// 			}
+// 			fileHelper.deleteFile(prod.imageUrl);
+// 			return Product.deleteOne({ _id: prodId, userId: req.user._id });
+// 		})
+// 		.then(() => {
+// 			console.log("DESTROYED PRODUCT");
+// 			res.redirect("/admin/products");
+// 		})
+// 		.catch(err => {
+// 			const error = new Error(err);
+// 			error.httpStatusCode = 500;
+// 			return next(error);
+// 		});
+// };
