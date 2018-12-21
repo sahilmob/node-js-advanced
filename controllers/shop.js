b1d72d96547390a1bcafa565e60181ec47f929a0
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-12-21 14:12:26 +0200

    render pagination dunamically

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,7 +5,7 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
 	Product.find()
@@ -42,16 +42,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-	const page = req.query.page;
-
+	const page = +req.query.page || 1;
+	let totalItems;
 	Product.find()
-		.skip((page - 1) * ITEMS_PER_PAGE)
-		.limit(ITEMS_PER_PAGE)
+		.countDocuments()
+		.then(numProducts => {
+			totalItems = numProducts;
+			return Product.find()
+				.skip((page - 1) * ITEMS_PER_PAGE)
+				.limit(ITEMS_PER_PAGE);
+		})
 		.then(products => {
 			res.render("shop/index", {
 				prods: products,
 				pageTitle: "Shop",
-				path: "/"
+				path: "/",
+				totalProducts: totalItems,
+				currentPage: page,
+				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+				hasPrevPage: page > 1,
+				nextPage: page + 1,
+				prevPage: page - 1,
+				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
 			});
 		})
 		.catch(err => {
